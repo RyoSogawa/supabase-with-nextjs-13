6f85c041a8ae0b6894fb5c4ea45a14fba544866e
RyoSogawa <koashimitekara.1122@gmail.com>
2022-11-05 15:30:29 +0900

    update supabase client type

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient(
+import type { Database } from './database.types';
+
+export const supabase = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL ?? 'http://localhost:54321',
   process.env.SUPABASE_SERVICE_ROLE_KEY ??
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSJ9.vI9obAHOGyVVKa3pD--kJlyxp-Z2zV9UUMAhKpNLAcU',
